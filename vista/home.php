2aa2a4d3d6fe0446f479e76398d2c37c6d0add93
Carlos Menares <menaresdesarrollo@gmail.com>
2023-04-24 20:39:53 -0400

    Primera carga de proyecto

---
+++ vista/home.php
@@ -0,0 +1,16 @@
+<?php
+    $nom = $_POST['n'];
+    ECHO "El nombre es: ".$nom."<br>";
+
+    $apellido = $_POST['a'];
+    ECHO "El apellido es: ".$apellido."<br>";
+
+    $rut = $_POST['r'];
+    ECHO "El rut es: ".$nom."<br>";
+
+    $edad = $_POST['edad'];
+    ECHO "la edad es: ".$edad."<br>";
+
+    $contrasena = $_POST['contrasena'];
+    ECHO "La contrasena es: ".$contrasena."<br>";
+?>
---
+++ vista/index.php
@@ -0,0 +1,37 @@
+<script>
+function soloNumeros(e) {
+var key = window.Event ? e.which : e.keyCode
+    return (key >= 48 && key <= 57)
+}
+</script>
+
+<html>
+<head>
+<title> FORMULARIO </title>
+</head>
+<body>
+
+----- INGRESE SUS DATOS ----- <br><br>
+           
+<form id="formulario" action="home.php" method="POST" >
+
+Nombre
+<input type="text" id="n" name="n"><br><br>
+
+Apellido
+<input type="text" id="a" name="a"><br><br>
+
+Rut
+<input type="text" id="r" name="r" onkeypress="return soloNumeros(event)"><br><br>
+
+Edad
+<input type="text" id="edad" name="edad"><br><br>
+
+CONTRASEÑA
+<input type="password" id="contrasena" name="contrasena"><br><br>
+<button type="submit">enviar</button>
+</form>
+
+</body>
+
+</html>
